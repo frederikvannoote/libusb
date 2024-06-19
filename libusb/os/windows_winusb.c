9364ac575e8de3226564cd60f91613120c4695c5
Frederik Vannoote <frederik.vannoote@barco.com>
2024-06-19 15:44:10 +0200

    Adapt code for HID_API

--- libusb/os/windows_winusb.c
+++ libusb/os/windows_winusb.c
@@ -4713,14 +4713,14 @@ static int composite_reset_device(int sub_api, struct libusb_device_handle *dev_
 		available[i] = false;
 
 	for (i = 0; i < USB_MAXINTERFACES; i++) {
-		if ((priv->usb_interface[i].apib->id == USB_API_WINUSBX)
-				&& (priv->usb_interface[i].sub_api != SUB_API_NOTSET))
+		if (((priv->usb_interface[i].apib->id == USB_API_WINUSBX) || (priv->usb_interface[i].apib->id == USB_API_HID))
+			&& (priv->usb_interface[i].sub_api != SUB_API_NOTSET))
 			available[priv->usb_interface[i].sub_api] = true;
 	}
 
 	for (i = 0; i < SUB_API_MAX; i++) {
 		if (available[i]) {
-			r = usb_api_backend[USB_API_WINUSBX].reset_device(i, dev_handle);
+			r = usb_api_backend[USB_API_HID].reset_device(i, dev_handle);
 			if (r != LIBUSB_SUCCESS)
 				return r;
 		}
